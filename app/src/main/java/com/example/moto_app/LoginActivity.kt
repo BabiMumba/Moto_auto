6932eb334ad3efc8f196a5b1a8bf300792cb712e
Babi Mumba <babimumba243@gmail.com>
2022-07-24 21:09:12 +0200

    add a new file

--- app/src/main/java/com/example/moto_app/LoginActivity.kt
+++ app/src/main/java/com/example/moto_app/LoginActivity.kt
@@ -4,7 +4,9 @@ import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Patterns
+import android.widget.Toast
 import com.example.moto_app.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -20,12 +22,15 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
          progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Please wait")
         progressDialog.setMessage("loading in...")
         progressDialog.setCancelable(false)
         progressDialog.setCanceledOnTouchOutside(false)
+
         firebaseAuth = FirebaseAuth.getInstance()
+
         checkuser()
         binding.createCount.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
@@ -43,7 +48,29 @@ class LoginActivity : AppCompatActivity() {
         password = binding.passwordLogin.text.toString().trim()
         if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
             binding.mailLogin.setError("mail invalide")
+        }else if(TextUtils.isEmpty(password)){
+            binding.passwordLogin.error = "please enter the password"
+        }else{
+            firebaseLogin()
         }
+    }
+
+    private fun firebaseLogin() {
+        progressDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(mail,password)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                val firebase = firebaseAuth.currentUser
+                val email = firebase!!.email
+                Toast.makeText(this, "enregistrer avec succer", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,ProfilActivity::class.java))
+                finish()
+
+            }
+            .addOnFailureListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "erreur verifier votre connexion", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun checkuser() {
--- app/src/main/java/com/example/moto_app/SignUpActivity.kt
+++ app/src/main/java/com/example/moto_app/SignUpActivity.kt
@@ -1,11 +1,47 @@
 package com.example.moto_app
 
+import android.app.ProgressDialog
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moto_app.databinding.ActivityLoginBinding
+import com.example.moto_app.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var mail = ""
+    private var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("creation du compte")
+        progressDialog.setCancelable(false)
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnRegister.setOnClickListener {
+            valideteData()
+        }
+    }
+
+    private fun valideteData() {
+        mail = binding.mailRegister.text.toString().trim()
+        password = binding.passwordRegister.text.toString().trim()
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
