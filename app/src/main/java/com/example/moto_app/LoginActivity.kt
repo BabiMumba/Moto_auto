e0e4d14da5ce9b849b0baf2dff6d0b35847712a7
Babi Mumba <babimumba243@gmail.com>
2022-07-24 20:51:01 +0200

    add a new file

--- app/src/main/java/com/example/moto_app/LoginActivity.kt
+++ app/src/main/java/com/example/moto_app/LoginActivity.kt
@@ -1,11 +1,32 @@
 package com.example.moto_app
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moto_app.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+         progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("loading in...")
+        progressDialog.setCancelable(false)
+        progressDialog.setCanceledOnTouchOutside(false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkuser()
+
+    }
+
+    private fun checkuser() {
+
     }
 }
