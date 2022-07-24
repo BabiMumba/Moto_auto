5d24aab85c7ce3103c29a08ef8255e16a1a2923f
Babi Mumba <babimumba243@gmail.com>
2022-07-24 20:57:32 +0200

    add  a nw code file

--- app/src/main/java/com/example/moto_app/LoginActivity.kt
+++ app/src/main/java/com/example/moto_app/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.moto_app
 
 import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.example.moto_app.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
@@ -11,6 +13,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var progressDialog: ProgressDialog
     private lateinit var firebaseAuth: FirebaseAuth
+    private var mail = ""
+    private var password = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +27,31 @@ class LoginActivity : AppCompatActivity() {
         progressDialog.setCanceledOnTouchOutside(false)
         firebaseAuth = FirebaseAuth.getInstance()
         checkuser()
+        binding.createCount.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+        binding.btnLogin.setOnClickListener {
+
+            validatedate()
+        }
 
     }
 
+    private fun validatedate() {
+
+        mail = binding.mailLogin.text.toString().trim()
+        password = binding.passwordLogin.text.toString().trim()
+        if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+            binding.mailLogin.setError("mail invalide")
+        }
+    }
+
     private fun checkuser() {
+        val firebaseuser = firebaseAuth.currentUser
+        if (firebaseuser != null){
+            startActivity(Intent(this,ProfilActivity::class.java))
+            finish()
+        }
 
     }
 }
