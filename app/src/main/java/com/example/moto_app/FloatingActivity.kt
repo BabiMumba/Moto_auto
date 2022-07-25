48262100e134787406cc965768c8da34b05a28f8
Babi Mumba <babimumba243@gmail.com>
2022-07-25 12:54:44 +0200

    add anew filen

--- app/src/main/java/com/example/moto_app/FloatingActivity.kt
+++ app/src/main/java/com/example/moto_app/FloatingActivity.kt
@@ -2,14 +2,18 @@ package com.example.moto_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.example.moto_app.R
+import kotlinx.android.synthetic.main.activity_floating.*
 
 class FloatingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_floating)
 
-        bottomAppBar.setOnMenuItemClickListener { item: MenuItem ->
+        bottom_app_bar.setOnMenuItemClickListener { item: MenuItem ->
             when (item.itemId) {
                 R.id.search -> Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT)
                     .show()
