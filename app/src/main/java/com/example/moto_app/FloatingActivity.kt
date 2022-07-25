7d6383ebc576c5f9ad41e5a86e6dfe2d8ee7f328
Babi Mumba <babimumba243@gmail.com>
2022-07-25 12:52:58 +0200

    add a new file

--- app/src/main/java/com/example/moto_app/FloatingActivity.kt
+++ app/src/main/java/com/example/moto_app/FloatingActivity.kt
@@ -8,5 +8,19 @@ class FloatingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_floating)
+
+        bottomAppBar.setOnMenuItemClickListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.search -> Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT)
+                    .show()
+                R.id.more -> Toast.makeText(this, "More Clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            false
+        }
+
+        findViewById<View>(R.id.fab).setOnClickListener {
+            Toast.makeText(this, "Fab Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
