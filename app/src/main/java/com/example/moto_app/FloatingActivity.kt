d3f68ddad85732fe175983fbfeb0d2b798e5519e
Babi Mumba <babimumba243@gmail.com>
2022-07-25 12:49:01 +0200

    new file

---
+++ app/src/main/java/com/example/moto_app/FloatingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moto_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moto_app.R
+
+class FloatingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_floating)
+    }
+}
