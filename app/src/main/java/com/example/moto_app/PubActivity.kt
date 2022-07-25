d193a112fa1041ca81a84cdcfe6e7469ab33e4ea
Babi Mumba <babimumba243@gmail.com>
2022-07-25 09:58:15 +0200

    add a new file

--- app/src/main/java/com/example/moto_app/PubActivity.kt
+++ app/src/main/java/com/example/moto_app/PubActivity.kt
@@ -1,12 +1,21 @@
 package com.example.moto_app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import kotlinx.android.synthetic.main.activity_pub.*
 
 class PubActivity : AppCompatActivity() {
+    private var mInterstitialAd: InterstitialAd? = null
+
+    //bannier
     lateinit var adView: AdView
     lateinit var adRequest: AdRequest
 
@@ -28,5 +37,26 @@ class PubActivity : AppCompatActivity() {
         // ad view with the ad request
         adView.loadAd(adRequest)
 
+
+        //intersecel
+        var adRequest = AdRequest.Builder().build()
+
+        video()
+
+
+    }
+
+    private fun video() {
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.toString())
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
     }
 }
