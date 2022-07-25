0ce12700f73ad7307312feebcd48c3e431a92d99
Babi Mumba <babimumba243@gmail.com>
2022-07-25 10:59:07 +0200

    add a new file

---
+++ app/src/main/java/com/example/moto_app/DownloadActivity_kotline.kt
@@ -0,0 +1,63 @@
+package com.example.moto_app
+
+import android.Manifest
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moto_app.R
+import android.content.pm.PackageManager
+import android.app.DownloadManager
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.util.Log
+import android.view.View
+import android.webkit.*
+import android.widget.Toast
+
+class DownloadActivity_kotline : AppCompatActivity() {
+
+    var url = "https://drive.google.com/file/d/0B_rn9jkskDivX1dfZ3B3M2JVX2M/view?usp=drive_web"
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_download)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED
+            ) {
+                Log.d("permission", "permission denied to WRITE_EXTERNAL_STORAGE - requesting it")
+                val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permissions, 1)
+            }
+        }
+
+        var webView: WebView = findViewById(R.id.web_view)
+        webView.webViewClient = WebViewClient()
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.javaScriptEnabled = true
+        webView.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        webView.loadUrl(url)
+        webView.setDownloadListener(DownloadListener { url, userAgent, contentDisposition, mimeType, contentLength ->
+            val request = DownloadManager.Request(
+                Uri.parse(url)
+            )
+            request.setMimeType(mimeType)
+            val cookies = CookieManager.getInstance().getCookie(url)
+            request.addRequestHeader("cookie", cookies)
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading File...")
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                    url, contentDisposition, mimeType
+                )
+            )
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
+        })
+    }
+}
