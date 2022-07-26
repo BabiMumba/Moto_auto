eb4cba57a956b519ba3b928df59b583e26832fcc
Babi Mumba <babimumba243@gmail.com>
2022-07-26 22:37:16 +0200

    add a lifescale

--- app/src/main/java/com/example/moto_app/Admob.kt
+++ app/src/main/java/com/example/moto_app/Admob.kt
@@ -3,9 +3,7 @@ package com.example.moto_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.ads.*
 
 class Admob : AppCompatActivity() {
     private companion object{
@@ -24,7 +22,59 @@ class Admob : AppCompatActivity() {
                 .setTestDeviceIds(listOf("PLACE_TESTE_DEVICE_ID_1_HERE","PLACE_TESTE_DEVICE_ID_2_HERE"))
                 .build()
         )
+        adview = findViewById(R.id.bannerAd)
+        val adRequest = AdRequest.Builder().build()
+        //load ad
+        adview?.loadAd(adRequest)
+        //setup ad callback listener
 
+        adview?.adListener = object : AdListener(){
+            override fun onAdClicked() {
+                super.onAdClicked()
+                Log.d(TAG, "onAdClicked: ")
+            }
 
+            override fun onAdClosed() {
+                super.onAdClosed()
+                Log.d(TAG, "onAdClosed: ")
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                super.onAdFailedToLoad(p0)
+                Log.d(TAG, "onAdFailedToLoad: $p0")
+            }
+
+            override fun onAdImpression() {
+                super.onAdImpression()
+                Log.d(TAG, "onAdImpression: ")
+            }
+
+            override fun onAdLoaded() {
+                super.onAdLoaded()
+                Log.d(TAG, "onAdLoaded: ")
+            }
+
+            override fun onAdOpened() {
+                super.onAdOpened()
+                Log.d(TAG, "onAdOpened: ")
+            }
+        }
+    }
+
+    override fun onPause() {
+        adview?.pause()
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+
+    }
+    override fun onResume() {
+        adview?.resume()
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+    override fun onDestroy() {
+        adview?.destroy()
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
     }
 }
