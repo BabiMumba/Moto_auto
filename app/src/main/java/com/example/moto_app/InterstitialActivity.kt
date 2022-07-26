81eac5800f27c22e3530ba8d11d9cefc3a03f377
Babi Mumba <babimumba243@gmail.com>
2022-07-26 23:38:21 +0200

    add a new file

--- app/src/main/java/com/example/moto_app/InterstitialActivity.kt
+++ app/src/main/java/com/example/moto_app/InterstitialActivity.kt
@@ -3,10 +3,8 @@ package com.example.moto_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
+import android.widget.Toast
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.google.android.material.button.MaterialButton
@@ -45,6 +43,8 @@ class InterstitialActivity : AppCompatActivity() {
 
                 override fun onAdFailedToLoad(p0: LoadAdError) {
                     super.onAdFailedToLoad(p0)
+                    Log.d(TAG, "onAdFailedToLoad: ")
+                    mInterstitialAd = null
                 }
 
                 override fun onAdLoaded(p0: InterstitialAd) {
@@ -55,4 +55,38 @@ class InterstitialActivity : AppCompatActivity() {
             }
         )
     }
+    private fun showInterstitialAd(){
+        if (mInterstitialAd != null){
+            Log.d(TAG,"showInterstitial: Ad was loaded, we can show")
+            mInterstitialAd!!.setFullScreenContentCallback(object : FullScreenContentCallback() {
+                override fun onAdClicked() {
+                    super.onAdClicked()
+                }
+
+                override fun onAdDismissedFullScreenContent() {
+                    super.onAdDismissedFullScreenContent()
+                }
+
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    super.onAdFailedToShowFullScreenContent(p0)
+                }
+
+                override fun onAdImpression() {
+                    super.onAdImpression()
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                    super.onAdShowedFullScreenContent()
+                }
+            })
+            mInterstitialAd!!.show(this)
+        }else{
+            Log.d(TAG,"showInterstitial: Ad was not loaded cant show")
+            toast("Ad was not loaded task e.g start activity")
+        }
+    }
+    private fun toast(message:String){
+        Toast.makeText(this, "$message", Toast.LENGTH_SHORT).show()
+    }
+
 }
