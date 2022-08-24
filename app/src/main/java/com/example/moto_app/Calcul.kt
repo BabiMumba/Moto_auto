14735f70218299d07ff853575cb4d64915f69275
Babi Mumba <babimumba243@gmail.com>
2022-08-25 00:44:08 +0200

    add new file

--- app/src/main/java/com/example/moto_app/Calcul.kt
+++ app/src/main/java/com/example/moto_app/Calcul.kt
@@ -12,26 +12,34 @@ class Calcul : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcul)
 
+
+        teste()
+
+        verf.setOnClickListener {
+
+            val repos = reponse.text.toString()
+            if (repos == calculatrice.toString()){
+                score++
+                pts.text = score.toString()
+                teste()
+                reponse.setText("")
+            }
+            else
+            {
+                pts.text = score--.toString()
+                teste()
+                reponse.setText("")
+
+            }
+        }
+    }
+    private fun teste(){
         val chif1:Int = Random.nextInt(0,100)
         val chif2:Int = Random.nextInt(0,100)
         txt1.text = chif1.toString()
         txt2.text = chif2.toString()
         calculatrice = chif1 + chif2
-        verf.setOnClickListener {
-            val repos = reponse.text.toString()
-            if (repos == calculatrice.toString()){
-                score++
-                pts.text = score.toString()
-            }
-            else
-            {
-                pts.text = "echouer"
-                txt1.text = chif1.toString()
-                txt2.text = chif2.toString()
-                calculatrice = chif1 + chif2
 
-            }
-        }
     }
 
 }
