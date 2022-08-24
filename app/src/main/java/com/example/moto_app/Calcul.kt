976a77bfe2d19cc1c3eeda44f9ee44186035f6e4
Babi Mumba <babimumba243@gmail.com>
2022-08-25 00:19:14 +0200

    add new file

--- app/src/main/java/com/example/moto_app/Calcul.kt
+++ app/src/main/java/com/example/moto_app/Calcul.kt
@@ -2,10 +2,19 @@ package com.example.moto_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calcul.*
+import kotlin.random.Random
 
 class Calcul : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcul)
+
+        val chif1 = Random.nextInt(0,100)
+        val chif2 = Random.nextInt(0,100)
+        val resulta = chif1+chif2
+        verf.setOnClickListener {
+            val repos =
+        }
     }
 }
