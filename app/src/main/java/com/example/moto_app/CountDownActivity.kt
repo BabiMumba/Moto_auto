8d781c85bae20e0507ba28fe15e00cbefd1188b6
Babi Mumba <babimumba243@gmail.com>
2022-09-11 23:53:16 +0200

    add new file

--- app/src/main/java/com/example/moto_app/CountDownActivity.kt
+++ app/src/main/java/com/example/moto_app/CountDownActivity.kt
@@ -2,12 +2,20 @@ package com.example.moto_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_count_down.*
 
 class CountDownActivity : AppCompatActivity() {
+
+    var pts = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_count_down)
+        point.setText(pts)
+        ajoute.setOnClickListener {
+            pts++
+        }
+
 
     }
    }
