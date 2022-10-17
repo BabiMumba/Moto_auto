135b54a9a86e7d515279b8e4e00a51c656b6340f
Babi Mumba <babimumba243@gmail.com>
2022-10-17 22:28:02 +0200

    add new file

--- app/src/main/java/com/example/moto_app/Admob.kt
+++ app/src/main/java/com/example/moto_app/Admob.kt
@@ -20,7 +20,7 @@ class Admob : AppCompatActivity() {
         }
         MobileAds.setRequestConfiguration(
             RequestConfiguration.Builder()
-                .setTestDeviceIds(listOf("c0404e6b-d5c3-4498-89e3-7ae83f90e059","e4daca5f-a422-4831-b688-7b0b0d1da7cc"))
+               // .setTestDeviceIds(listOf("c0404e6b-d5c3-4498-89e3-7ae83f90e059","e4daca5f-a422-4831-b688-7b0b0d1da7cc"))
                 .build()
         )
         adview = findViewById(R.id.bannerAd)
